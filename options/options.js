3e9db0cb05455ce36f1a712641cf823756a11200
Crowdbotics <team@crowdbotics.com>
2024-12-11 22:16:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-haze-50299",
+  url: "https://calm-haze-50299.botics.co",
+  api: "https://calm-haze-50299.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
